f80f8c6bb4a54d508bd96a0f1d4aad9e611398e4
unknown <arifk62901@gmail.com>
2025-06-28 11:12:30 +0600

    cleark authentication added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton>
+          <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
